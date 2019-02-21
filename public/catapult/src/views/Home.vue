ca2e6ac97fbfe39026c114539df747fa9e2b0cf8
Benny Zlotnik <uber442@gmail.com>
2019-02-21 14:18:47 +0200

    fix JS issues

--- public/catapult/src/views/Home.vue
+++ public/catapult/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
+    <h1>Home</h1>
 </template>
 
 <script lang="ts">
+    import Home from '../views/Home';
+
+    const router = new VueRouter({
+    routes: [
+        {
+        path: '/',
+        name: 'Home',
+        component: Home
+        }
+    ]
+    })
+
 </script>
