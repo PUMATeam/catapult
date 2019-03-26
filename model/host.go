e97a08ac6a72079326b88424325f60b2b9b9935a
Benny Zlotnik <uber442@gmail.com>
2019-03-26 18:26:27 +0200

    introduce host addition support

--- model/host.go
+++ model/host.go
@@ -1,14 +1,15 @@
 package model
 
 import (
-	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
 )
 
 // Host represents a host in the system
 type Host struct {
-	ID      uuid.UUID `json:"id" gorm:"primary_key"`
-	Name    string    `json:"name"`
-	Address string    `json:"address"`
+	gorm.Model
+	ID      string `json:"id" gorm:"primary_key;type:uuid"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
 // GetHosts retrieves all the available hosts
@@ -17,4 +18,13 @@ func (h Host) GetHosts() []Host {
 	db.Find(&hosts)
 
 	return hosts
+}
+
+// CreateHost adds the host to database
+func (h Host) CreateHost(host Host) Host {
+	if db.NewRecord(host) {
+		db.Create(&host)
+	}
+
+	return host
 }
