8acf984db569a8acc4df5368949f9bec288dd791
Benny Zlotnik <uber442@gmail.com>
2019-06-03 10:39:00 +0300

    remove gorm

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/BurntSushi/toml"
 
 	"github.com/PUMATeam/catapult/api"
 	"github.com/PUMATeam/catapult/model"
 )
 
 func main() {
-	model.InitDB()
+	if pgOptions, err := toml.DecodeFile("db.toml", &dbConfig); err != nil {
+		log.Fatalf("Failed reading db config: %s", err)
+	}
+
+	db = model.Connect(pgOptions.database, pgOptions.username, pgOptions.password)
+	defer db.Close()
+
 	router := api.InitRoutes()
 	http.ListenAndServe(":3333", router)
 }
--- model/host.go
+++ model/host.go
@@ -1,30 +1,20 @@
 package model
 
-import (
-	"github.com/jinzhu/gorm"
-)
-
 // Host represents a host in the system
 type Host struct {
-	gorm.Model
-	ID      string `json:"id" gorm:"primary_key;type:uuid"`
+	ID      string `json:"id"`
 	Name    string `json:"name"`
 	Address string `json:"address"`
 }
 
 // GetHosts retrieves all the available hosts
 func (h Host) GetHosts() []Host {
-	var hosts []Host
-	db.Find(&hosts)
 
 	return hosts
 }
 
 // CreateHost adds the host to database
 func (h Host) CreateHost(host Host) Host {
-	if db.NewRecord(host) {
-		db.Create(&host)
-	}
 
 	return host
 }
