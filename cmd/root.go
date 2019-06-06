7acce61f02b092be02d5d76ad96349fb71e0c87e
Benny Zlotnik <uber442@gmail.com>
2019-06-06 09:34:35 +0300

    start the server

--- cmd/root.go
+++ cmd/root.go
@@ -62,9 +62,8 @@ func initConfig() {
 		viper.SetConfigName(".catapult")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
---
+++ cmd/server.go
@@ -0,0 +1,40 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/PUMATeam/catapult/api"
+	"github.com/spf13/cobra"
+)
+
+var (
+	port int
+)
+
+// restCmd represents the rest command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start catapult server",
+	Long:  `Start catapult server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		api.Start(port)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	serverCmd.Flags().IntVarP(&port, "port", "p", 8888, "Port for which to listen")
+}
