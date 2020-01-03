d599a4d3ccd4b77c64198e6a8600082cada0effa
Benny Zlotnik <uber442@gmail.com>
2020-01-03 19:06:34 +0200

    set static ip to bridge
    Do not rely on dhcp to set eth0 IP
    
    fixes #127

--- ansible/roles/setup_host/tasks/files/setup_bridge.sh
+++ ansible/roles/setup_host/tasks/files/setup_bridge.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-brctl addbr fcbridge
-ip addr flush dev eth0
-brctl addif fcbridge eth0
-dhclient -v fcbridge
+IP=$(hostname -I)
+IP_WITH_CIDR=$(ip addr show dev eth0 | grep inet | awk '{print $2}')
+ROUTE=$(ip route list dev eth0 | grep -v default | cut -f 1 -d ' ')
+DEFAULT_ROUTE=$(ip route list dev eth0 | grep default | awk '{print $3}')
+
+ip link add fcbridge type bridge
 ip link set fcbridge up
+ip addr flush dev eth0
+ip link set eth0 master fcbridge
+ip addr add "${IP_WITH_CIDR}" brd + dev fcbridge
+ip route add default via "${DEFAULT_ROUTE}"
+ip route add "${ROUTE}"  dev fcbridge proto kernel scope link src "${IP}"
