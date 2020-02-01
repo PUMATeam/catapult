3cf50bfe8a5e05b32e8c7056aac17c76a5e5925d
Benny Zlotnik <uber442@gmail.com>
2020-02-01 13:08:00 +0200

    fix logs

--- pkg/services/volumes.go
+++ pkg/services/volumes.go
@@ -9,21 +9,21 @@ import (
 	"github.com/PUMATeam/catapult/pkg/repositories"
 	"github.com/PUMATeam/catapult/pkg/storage"
 	uuid "github.com/satori/go.uuid"
-	logger "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 type volumesService struct {
 	vmsRepository  repositories.VMs
 	hostsService   Hosts
 	storageService storage.Service
-	logger         *logger.Logger
+	log            *log.Logger
 }
 
-func NewVolumesService(hs Hosts, s storage.Service, logger *logger.Logger) Volumes {
+func NewVolumesService(hs Hosts, s storage.Service, logger *log.Logger) Volumes {
 	vls := &volumesService{
 		hostsService:   hs,
 		storageService: s,
-		logger:         logger,
+		log:            logger,
 	}
 	return vls
 }
@@ -38,15 +38,14 @@ func (v *volumesService) AddVolume(ctx context.Context, volume VolumeReq) (uuid.
 	nodeService := node.NewNodeService(h, v.hostsService.GetConnManager(ctx))
 	path, size, err := nodeService.CreateDrive(ctx, volume.ImageName)
 	if err != nil {
-		logger.
-			WithContext(ctx).
+		v.log.WithContext(ctx).
 			Error(err)
 		return uuid.Nil, err
 	}
 
 	// TODO: extract to util
 	volSize := int64(math.Ceil(float64(size) / (1024 * 1024 * 1024)))
-	logger.WithContext(ctx).Infof("Creating volume %v with size %s GiB",
+	v.log.WithContext(ctx).Infof("Creating volume %v with size %s GiB",
 		volID,
 		volSize)
 	_, err = v.storageService.Create(ctx, &storage.Volume{
@@ -55,19 +54,18 @@ func (v *volumesService) AddVolume(ctx context.Context, volume VolumeReq) (uuid.
 		Size: volSize,
 	})
 	if err != nil {
-		logger.
-			WithContext(ctx).
+		v.log.WithContext(ctx).
 			Error(err)
 		return uuid.Nil, err
 	}
 
-	logger.WithContext(ctx).WithField("path", path).Infof("Created drive")
+	v.log.WithContext(ctx).WithField("path", path).Infof("Created drive")
 
 	nodeService.ConnectVolume(ctx,
 		&node.Volume{VolumeID: volID.String(),
 			PoolName: "volumes"})
 
-	logger.WithContext(ctx).
+	v.log.WithContext(ctx).
 		WithField("volume", volID).
 		WithField("size", size).
 		Infof("Mapped volume")
